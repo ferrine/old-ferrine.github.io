b5fce79ec77ae571dbd9de0d717ed07838197099
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2022-01-30 17:11:32 +0000

    hover anchor

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,29 +1,35 @@
 from pelican_jupyter import markup as nb_markup
+import nbconvert
 
 MARKUP = ("md", "ipynb")
 
 PLUGINS = [nb_markup]
 IPYNB_SKIP_CSS = True
 IGNORE_FILES = [".ipynb_checkpoints"]
+IPYNB_PREPROCESSORS = [
+    nbconvert.preprocessors.TagRemovePreprocessor(
+        remove_cell_tags="remove_cell",
+        remove_all_outputs_tags="remove_all_outputs",
+        remove_single_output_tags="remove_single_output",
+        remove_input_tags="remove_input",
+    )
+]
 
+AUTHOR = "Maxim Kochurov"
+SITENAME = "In Search of the Holy Posterior"
+SITEURL = ""
 
-AUTHOR = 'Maxim Kochurov'
-SITENAME = 'In Search of the Holy Posterior'
-SITEURL = ''
-
-PATH = 'content'
-THEME = 'theme'
-MENUITEMS = (
-    ("Categories", "categories"),
-)
-COLOR_SCHEME_CSS = 'github.css'
+PATH = "content"
+THEME = "theme"
+MENUITEMS = (("Categories", "categories"),)
+COLOR_SCHEME_CSS = "github.css"
 HEADER_COVER = "images/header.jpg"
 GRAPHVIZ_COMPRESS = False
 
 
-TIMEZONE = 'Europe/Moscow'
+TIMEZONE = "Europe/Moscow"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -36,20 +42,24 @@ AUTHOR_FEED_RSS = None
 LINKS = ()
 
 # Social widget
-SOCIAL = (('twitter', 'https://twitter.com/ferrine96'),
-          ('github', 'https://github.com/ferrine'),
-          ('envelope', 'mailto:max.kochurov@pymc-labs.io'))
+SOCIAL = (
+    ("twitter", "https://twitter.com/ferrine96"),
+    ("github", "https://github.com/ferrine"),
+    ("envelope", "mailto:max.kochurov@pymc-labs.io"),
+)
 
 DEFAULT_PAGINATION = 10
 
 STATIC_PATHS = [
-    'images',
-    'extra',  # this
+    "images",
+    "extra",  # this
 ]
 
 EXTRA_PATH_METADATA = {
-    'extra/favicon.ico': {'path': 'favicon.ico'},  # and this
+    "extra/favicon.ico": {"path": "favicon.ico"},  # and this
+    "extra/override.css": {"path": "theme/css/override.css"},
 }
+CSS_OVERRIDE = "theme/css/override.css"
 GOOGLE_ANALYTICS = "UA-98661135-1"
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
